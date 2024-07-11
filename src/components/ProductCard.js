c6857220e4603b42504ee19e8b76b3d8d5c8faf3
Pintaram <pintu.borana20@gmail.com>
2024-07-11 11:20:04 +0530

    ProductCard_frontend

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,9 +3,11 @@ import React from 'react';
 const ProductCard = ({ product }) => {
   return (
     <div className="product-card">
-      <h2>{product.name}</h2>
-      <p>{product.description}</p>
+      <h2>{product.productname}</h2>
       <p>Price: ${product.price}</p>
+      <p>Rating: ${product.rating}</p>
+      <p>Discount: ${product.discount}</p>
+      <p>{product.availablity}</p>
     </div>
   );
 };
